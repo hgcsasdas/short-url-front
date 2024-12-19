27f0857b9b703233ab748329f71c22ed41e9a428
Carlos Sanchez Núñez <carlos.sanchez@inmasan.com>
2024-12-19 17:08:28 +0100

    cambios al footer y al fondo

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -21,12 +21,12 @@ import { UrlFormComponent } from '../url-form/url-form.component';
   ],
   template: `
     <app-header></app-header>
-    <div class="h-[90vh]">
+    <div class="h-[85vh] bg-emerald-800 ">
       <app-url-form></app-url-form>
       <app-api-response></app-api-response>
       <app-demo></app-demo>
+      <app-footer></app-footer>
     </div>
-    <app-footer></app-footer>
   `,
   styles: [],
 })
